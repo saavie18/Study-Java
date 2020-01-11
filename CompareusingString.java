3a2b3e939f5bef7b4044d771e78474ec2e425ed6
Savitri Iyer <51967426+saavie18@users.noreply.github.com>
2020-01-11 17:21:29 +0530

    Create CompareusingString.java

---
+++ CompareusingString.java
@@ -0,0 +1,15 @@
+package com.jetbrains.Task1;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CompareusingString {
+    public static void main(String[] args) {
+        Set<String> str = new TreeSet<String>();
+        str.add("Mango");
+        str.add("Banana");
+        str.add("Apple");
+        str.add("Watermelon");
+        System.out.println(str);
+    }
+}
